e25fc53d50e4d85b8e8fbd2598c3bacc69690a99
Naufal Ihsan <92091542+nafxyy@users.noreply.github.com>
2023-03-06 13:21:33 +0800

    Add files via upload

---
+++ Posttest1/pt1/Edit.java
@@ -0,0 +1,15 @@
+package pt1;
+
+public class Edit {
+    void lama(){
+        System.out.println("=================================");
+        System.out.println("Pilih Index Data Yang Ingin Diubah: ");
+        System.out.println("=================================");
+    }
+    void selamat(){
+        System.out.println("=================================");
+        System.out.println("Data Pemain Berhasil Diperbarui!");
+        System.out.println("=================================");
+        
+    }
+}
---
+++ Posttest1/pt1/datapemain.java
@@ -0,0 +1,13 @@
+package pt1;
+
+public class datapemain {
+    String nama, tim;
+    int nopung, umur;
+    
+    public datapemain(String nama, String tim, int nopung, int umur){
+        this.nama = nama;
+        this.tim = tim;
+        this.nopung = nopung;
+        this.umur = umur;
+    }
+}
